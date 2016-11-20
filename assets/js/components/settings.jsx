4d85e19f05df31a1b1d447556ea00ec6e0732c1f
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-11-20 11:46:36 +0000

    settings.jsx: Add class for group of checkboxes
    DRY.

--- assets/js/components/settings.jsx
+++ assets/js/components/settings.jsx
@@ -1,54 +1,45 @@
 var React = require('react')
 
 
+var ChordGroup = React.createClass({
+  propTypes: {
+    class: React.PropTypes.string.isRequired,
+    options: React.PropTypes.array.isRequired,
+  },
+  render: function () {
+    return (
+      <div className={this.props.class}>
+        <div className="inline-checkbox">
+          <label className="checkbox">
+          <input type="checkbox"/> {this.props.options[0]}
+          </label>
+        </div>
+        <div className="inline-checkbox">
+          <label className="checkbox">
+            <input type="checkbox"/> {this.props.options[1]}
+          </label>
+        </div>
+        <div className="inline-checkbox">
+          <label className="checkbox">
+            <input type="checkbox"/> {this.props.options[2]}
+          </label>
+        </div>
+        <div className="inline-checkbox">
+          <label className="checkbox">
+            <input type="checkbox"/> {this.props.options[3]}
+          </label>
+        </div>
+      </div>
+    )
+  }
+})
+
 var Settings = React.createClass({
   render: function () {
     return (
       <div className="settings">
-        <div className="std-chords">
-          <div className="inline-checkbox">
-            <label className="checkbox">
-            <input type="checkbox"/> min
-            </label>
-          </div>
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> maj
-            </label>
-          </div>
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> dim
-            </label>
-          </div>
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> aug
-            </label>
-          </div>
-        </div>
-        <div className="7th-chords">
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> 7<sup>th</sup>
-            </label>
-          </div>
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> 9<sup>th</sup>
-            </label>
-          </div>
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> 11<sup>th</sup>
-            </label>
-          </div>
-          <div className="inline-checkbox">
-            <label className="checkbox">
-              <input type="checkbox"/> 13<sup>th</sup>
-            </label>
-          </div>
-        </div>
+        <ChordGroup class="std-chords" options={['maj', 'min', 'dim', 'aug']}/>
+        <ChordGroup class="7th-chords" options={['7th', '9th', '11th', '13th']}/>
         <div className="mode-select">
           <button className="btn btn-default">Change Mode</button>
           <p>Mode Name</p>
