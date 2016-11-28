b200835e142d94fa56d423342f3a23a243c3020a
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-11-28 17:29:07 +0000

    card: Add ability to Flip bottom half of card
    When clicking 'Flip', the answer will show. When the bottom half
    is clicked again, 'Flip' will show again.
    
    Highlighting is disabled in cases where users might click rapidly.

--- assets/js/components/card.jsx
+++ assets/js/components/card.jsx
@@ -1,4 +1,5 @@
 var React = require('react')
+require('../../css/card.scss')
 
 
 var SimpleLI = React.createClass({
@@ -27,12 +28,28 @@ var Answer = React.createClass({
   getInitialState: function () {
     return {
       text: 'Flip',
-      answer: 'C, E, G',
+      clickCount: 0,
     }
+  },
+  onClick: function () {
+    let clickCount = this.state.clickCount % 2
+    if (clickCount === 0) {
+      this.showAnswer()
+    } else {
+      this.showFlip()
+    }
+    this.setState({'clickCount': clickCount + 1})
+  },
+  showAnswer: function () {
+    this.setState({'text': 'C, E, G'})
+  },
+  showFlip: function() {
+    this.setState({'text': 'Flip'})
   },
   render: function () {
     return (
-      <SimpleLI text={this.state.text}/>
+      <li className="list-group-item noSelect" unselectable="on"
+          onClick={this.onClick}>{this.state.text}</li>
     )
   }
 })
