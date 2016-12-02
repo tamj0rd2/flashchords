9f87bf864b5fac55412c4d8a8050f6cf8f031ad4
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-12-02 17:21:51 +0000

    qa: Add functions to generate a new chord

--- assets/js/scripts/qa.jsx
+++ assets/js/scripts/qa.jsx
@@ -1,9 +1,43 @@
+var R = require('ramda')
+var rand = require('unique-random')
+var randArr = require('unique-random-array')
 var tonal = require('tonal')
 
-function getAnswer(question) {
-  return tonal.chord(question)
+
+const roots = R.range(60, 72).map(note => midiToNote(note))
+const randRootGen = rand(60, 71)
+const chordNames = tonal.chord.names()
+const randChordName = randArr(chordNames)
+
+
+function midiToNote(note) {
+  return tonal.note.pc(tonal.note.fromMidi(note))
+}
+
+function getAnswer(type, tonic) {
+  return tonal.chord.get(type, tonic)
+}
+
+function randRoot() {
+  return midiToNote(randRootGen())
+}
+
+function newQuestion() {
+  var tonic = randRoot()
+  var type = randChordName()
+  return {
+    tonic: tonic,
+    type: type,
+    answer: getAnswer(type, tonic)
+  }
 }
 
 module.exports = {
+  roots: roots,
   getAnswer: getAnswer,
+  newQuestion: newQuestion,
+  randRoot: randRoot,
+  randChordName: randChordName,
+  midiToNote: midiToNote,
+  chordNames: chordNames,
 }
--- assets/js/tests/qa.spec.js
+++ assets/js/tests/qa.spec.js
@@ -1,13 +1,103 @@
 var QA = require('../scripts/qa.jsx')
+var tonal = require('tonal')
 
 describe('QA', function () {
 
   describe('getAnswer', function () {
     it('should return an array of notes when given a chord name', function () {
-      var question = 'C'
-      var expected = ['C', 'E', 'G']
-      var answer = QA.getAnswer(question)
+      var question, expected, answer
+      question = {type: 'm', tonic: 'C'}
+      expected = ['C', 'Eb', 'G']
+      answer = QA.getAnswer(question.type, question.tonic)
       expect(answer).toEqual(expected)
+
+      question = {type: '', tonic: 'A'}
+      expected = ['A', 'C#', 'E']
+      answer = QA.getAnswer(question.type, question.tonic)
+      expect(answer).toEqual(expected)
+
+      question = {type: '11b9', tonic: 'D'}
+      expected = ['D', 'A', 'C', 'Eb', 'G']
+      answer = QA.getAnswer(question.type, question.tonic)
+      expect(answer).toEqual(expected)
+    })
+  })
+
+  describe('midiToNote', function () {
+    it('should convert a midi value to a note', function () {
+      var note = QA.midiToNote(60)
+      expect(note).toEqual('C')
+    })
+  })
+
+  describe('roots', function () {
+    var roots = QA.roots
+    it('should start at C and end at B', function () {
+      expect(roots[0]).toBe('C')
+      expect(roots[roots.length - 1]).toBe('B')
+    })
+    it('should contain 12 values', function () {
+      expect(roots.length).toBe(12)
+    })
+  })
+
+  describe('randRoot', function () {
+    it('should return a root note', function () {
+      var root = QA.randRoot()
+      expect(QA.roots).toContain(root)
+    })
+    it('should not return the same root twice in a row', function () {
+      var lastRoot, newRoot
+      for(var i = 0; i < 50; i++) {
+        newRoot = QA.randRoot()
+        if (lastRoot === newRoot) {
+          expect(lastRoot).not.toEqual(newRoot)
+          break
+        }
+        lastRoot = newRoot
+      }
+    })
+  })
+
+  describe('randChordName', function () {
+    it('should return a chord name', function () {
+      var chordName = QA.randChordName()
+      expect(tonal.chord.names()).toContain(chordName)
+    })
+    it('should not return the same name twice in a row', function () {
+      var lastChord, newChord
+      for(var i = 0; i < 50; i++) {
+        newChord = QA.randChordName()
+        if (lastChord === newChord) {
+          expect(lastChord).not.toEqual(newChord)
+          break
+        }
+        lastChord = newChord
+      }
+    })
+  })
+
+  describe('newQuestion', function () {
+    it('should return an array with a type, tonic and answer', function () {
+      var question = QA.newQuestion()
+      expect(QA.roots).toContain(question.tonic)
+      expect(QA.chordNames).toContain(question.type)
+      expect(question.answer).not.toEqual('')
+    })
+    it('should return a dict', function () {
+      var output = QA.newQuestion()
+      expect(typeof(output)).toBe('object')
+    })
+
+    it('should only return chords that have answers', function () {
+      var result
+      for(var i = 0; i < 100; i++) {
+        result = QA.newQuestion().answer
+        if (result.length === 0) {
+          expect(result.length).not.toEqual(0)
+          break
+        }
+      }
     })
   })
 })
