11ab52dba3457124fb9812f37f5d82cc8725ef8c
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-02-05 13:33:11 +0000

    Reset card on group change

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -56,6 +56,7 @@ var App = React.createClass({
     var newVals = this.state.checkboxVals
     newVals[boxIndex] = newState
     this.setState({checkboxVals: newVals})
+    this.resetCard()
   },
   render: function() {
     return (
